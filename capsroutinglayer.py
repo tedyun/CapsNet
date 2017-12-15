a87806fc98237fc34a95089e2a1bde06a2f94d9b
Ted Yun <ted.yun@oracle.com>
2017-12-14 21:15:20 -0500

    change variable names

--- capsroutinglayer.py
+++ capsroutinglayer.py
@@ -9,9 +9,9 @@ from tf.keras.engine.topology import Layer
 from tf.keras import backend as K
 # import matplotlib.pyplot as plt
 
-def squash(X, axis = None):
-    X_norm = tf.norm(X, axis = axis, keep_dims = True)
-    return X_norm / (1 + tf.square(X_norm)) * X
+def squash(x, axis = None):
+    x_norm = tf.norm(x, axis = axis, keep_dims = True)
+    return x_norm / (1 + tf.square(x_norm)) * x
 
 class CapsRoutingLayer(Layer):
     def __init__(self, n_output, dim_output, n_routing = 3, **kwargs):
@@ -33,28 +33,28 @@ class CapsRoutingLayer(Layer):
             initializer = 'glorot_uniform', trainable = True)
         super(CapsRoutingLayer, self).build(input_shape)
     
-    def call(self, X):
-        # X.shape = (None, n_input, dim_input)
-        X_reshape = K.reshape(X, (-1, self.n_input, 1, self.dim_input))
-        X_tile = K.tile(X_reshape, (1, 1, self.n_output, 1))
-        # X_tiled.shape = (None, n_input, n_output, dim_input)
-        X_hat = K.map_fn(lambda x : K.batch_dot(self.W, x, axes = [3, 2]), elems = X_tile)
-        # X_hat.shape = (None, n_input, n_output, dim_output)
-        X_hat_forward_only = K.stop_gradient(X_hat)
+    def call(self, x):
+        # x.shape = (None, n_input, dim_input)
+        x_reshape = K.reshape(x, (-1, self.n_input, 1, self.dim_input))
+        x_tile = K.tile(x_reshape, (1, 1, self.n_output, 1))
+        # x_tiled.shape = (None, n_input, n_output, dim_input)
+        x_hat = K.map_fn(lambda x : K.batch_dot(self.W, x, axes = [3, 2]), elems = x_tile)
+        # x_hat.shape = (None, n_input, n_output, dim_output)
+        x_hat_forward_only = K.stop_gradient(x_hat)
         b = K.zeros(shape = (None, self.n_input, self.n_output)) # TODO: does this work?
         v = None
         for iter in range(self.n_routing):
             c = tf.nn.softmax(b, dim = 2)
             # c.shape = b.shape = (None, n_input, n_output)
             if iter == self.n_routing - 1:
-                s = K.batch_dot(c, X_hat, [1, 1])
+                s = K.batch_dot(c, x_hat, [1, 1])
                 v = squash(s)
                 # s.shape = v.shape = (None, n_output, dim_output)
             else:
-                s = K.batch_dot(c, X_hat_forward_only, [1, 1])
+                s = K.batch_dot(c, x_hat_forward_only, [1, 1])
                 v = squash(s)
                 # s.shape = v.shape = (None, n_output, dim_output)
-                b = b + K.batch_dot(X_hat_forward_only, v, [3, 2])
+                b = b + K.batch_dot(x_hat_forward_only, v, [3, 2])
                 # b.shape = (None, n_input, n_output)
         return v
     
@@ -65,8 +65,8 @@ class CapsLengthLayer(Layer):
     """
     Outputs the lengths of capsule vectors. Has no weights.
     """
-    def call(self, X):
-        # X.shape = (None, n_input, dim_input)
-        return tf.norm(X, axis = 2, keep_dims = False)
+    def call(self, x):
+        # x.shape = (None, n_input, dim_input)
+        return tf.norm(x, axis = 2, keep_dims = False)
     def compute_output_shape(self, input_shape):
         return input_shape[:-1]
